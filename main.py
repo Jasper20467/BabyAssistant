cd868bf4f69cbc414bd9c6c85d6de4f6d2f51a7f
jasper <jasper20467@hotmail.com>
2024-07-14 20:32:31 +0800

    Update Line test token

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 app = Flask(__name__)
 
 # 以下請替換為您的 Channel Secret 和 Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('m8deMq/iaZ56h+wbHflTmFIu5JXPu4hOi0Q2yKW5vwQy4W3RsL5K/I8nErk7Pb++nY2Ijl52FdCqMyZQuBNewok8Nv+Jy02FIeMZWtCR4zBWpzY88EKuEpOzNc7bY9kLmbPDmIW0DgCNYLLdmBDbCwdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('58abdf1b41b1580ddd1b43e1f17cab96')
 
 @app.route("/callback", methods=['POST'])
 def callback():
